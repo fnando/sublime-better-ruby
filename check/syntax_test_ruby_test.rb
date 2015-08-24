1643c5e37f701231dd7da9994e04d4f3286e6319
Nando Vieira <fnando.vieira@gmail.com>
2015-08-24 00:45:03 -0300

    Imported official Ruby syntax.
    Also redoing all the snippets and creating specific languages like Bundler and Ruby Test.

---
+++ check/syntax_test_ruby_test.rb
@@ -0,0 +1,13 @@
+# class UserTest < Minitest::Test
+#   test 'some test' do
+
+#   end
+
+#   def test_another_test
+#     assert_equal 1, 1
+#   end
+# end
+
+class AnotherTest < ActiveSupport::TestCase
+
+end
